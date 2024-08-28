d75cd3abc65299ea6de84258ee0907d1e3a879a3
suryatapa7 <suryatapabhattacharya7@gmail.com>
2024-08-28 18:42:41 +0530

    update Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,13 @@
 import { useHistory, Link } from 'react-router-dom';
+import { useState } from 'react';
 
 
 const Navbar = ({ searchText, setSearchText }) => {
 
 
   const history = useHistory();
+
+  const [menuOpen, setMenuOpen] = useState(false); // Add a new state variable to track the menu's open state
 
   const updateSearchText = (e) => {
     // console.log(e.target.value);
@@ -17,20 +20,28 @@ const Navbar = ({ searchText, setSearchText }) => {
     history.push(`/search?query=${searchText}`);
   }
 
+  const handleLinkClick = () => {
+    setSearchText("");
+  }
+
+  const handleToggleMenu = () => { // Add a new function to toggle the menu
+    setMenuOpen(!menuOpen);
+  }
+
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">Movie Browser🎥</Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <Link className="navbar-brand clickLink" to="/" onClick={handleLinkClick}>Movie Browser🎥</Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="true" aria-label="Toggle navigation" onClick={handleToggleMenu}>
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <div className={`navbar-collapse collapse ${menuOpen ? 'show' : ''}`} id="navbarSupportedContent"> 
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+              <Link className="nav-link active clickLink" aria-current="page" to="/" onClick={handleLinkClick}>Home</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/about">About</Link>
+              <Link className="nav-link clickLink" to="/about" onClick={handleLinkClick}>About</Link>
             </li>
 
             <li className="nav-item">
